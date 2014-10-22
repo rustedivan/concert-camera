f692191da5d0c88de26b9b70a7fb4383c4d17dd1
Ivan Milles <ivan@socialthrills.se>
2014-10-22 10:29:33 +0200

    Project: - removed GPUImage pod - added GPUImage as framework module instead

---
+++ concert-camera/concert-camera/ConcertCameraBridgingHeader.h
@@ -0,0 +1,11 @@
+//
+//  ConcertCameraBridgingHeader.h
+//  concert-camera
+//
+//  Created by Ivan Milles on 22/10/14.
+//  Copyright (c) 2014 Ivan Milles. All rights reserved.
+//
+
+#pragma once
+
+#import <GPUImage/GPUImage.h>
